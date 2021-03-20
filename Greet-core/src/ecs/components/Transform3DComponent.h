10fc071795cc2c265d318c6089d461bd02ea1031
Thraix <thraix@hotmail.com>
2021-03-21 00:04:29 +0100

    Add translation arrow functionality

--- Greet-core/src/ecs/components/Transform3DComponent.h
+++ Greet-core/src/ecs/components/Transform3DComponent.h
@@ -8,10 +8,26 @@ namespace Greet
 {
   struct Transform3DComponent
   {
-    Mat4 transform;
+    private:
+      Vec3f position{0, 0, 0};
+      Vec3f scale{1, 1, 1};
+      Vec3f rotation{0, 0, 0};
+    public:
+      Mat4 transform;
 
     Transform3DComponent(const Mat4& transform);
     Transform3DComponent(const Vec3f& pos, const Vec3f& scale, const Vec3f& rot);
     Transform3DComponent(const MetaFileClass& metaClass);
+
+    void SetPosition(const Vec3f& avPos);
+    void SetScale(const Vec3f& avScale);
+    void SetRotation(const Vec3f& avRotation);
+
+    const Vec3f& GetPosition() const;
+    const Vec3f& GetScale() const;
+    const Vec3f& GetRotation() const;
+
+    private:
+      void RecalcTransform();
   };
 }
