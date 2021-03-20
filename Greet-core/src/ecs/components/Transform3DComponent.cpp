3d8263b0036ab5b8d5f48a46047ee04d41bb1399
Thraix <thraix@hotmail.com>
2021-03-21 00:04:29 +0100

    Add translation arrow functionality

--- Greet-core/src/ecs/components/Transform3DComponent.cpp
+++ Greet-core/src/ecs/components/Transform3DComponent.cpp
@@ -18,4 +18,42 @@ namespace Greet
         MetaFileLoading::LoadVec3f(metaClass, "scale", {1.0f}),
         MetaFileLoading::LoadVec3f(metaClass, "rotation", {0.0f}).ToRadians())}
   {}
+
+  void Transform3DComponent::SetPosition(const Vec3f& avPos)
+  {
+    position = avPos;
+    RecalcTransform();
+  }
+
+  void Transform3DComponent::SetScale(const Vec3f& avScale)
+  {
+    scale = avScale;
+    RecalcTransform();
+  }
+
+  void Transform3DComponent::SetRotation(const Vec3f& avRotation)
+  {
+    position = avRotation;
+    RecalcTransform();
+  }
+
+  const Vec3f& Transform3DComponent::GetPosition() const
+  {
+    return position;
+  }
+
+  const Vec3f& Transform3DComponent::GetScale() const
+  {
+    return scale;
+  }
+
+  const Vec3f& Transform3DComponent::GetRotation() const
+  {
+    return rotation;
+  }
+
+  void Transform3DComponent::RecalcTransform()
+  {
+    transform = Mat4::TransformationMatrix(position, scale, rotation);
+  }
 }
